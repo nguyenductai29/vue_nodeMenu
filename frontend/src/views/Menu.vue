f71c918fe35d64d358731a8b9b2725a336b23eab
tai.nd <imochan1510@gmail.com>
2022-03-30 00:00:01 +0900

    create methods convert invalidSelectionTextToValue

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -75,35 +75,35 @@ export default {
             title: 'メニュー一覧',
             isDisabled: false,
             foods: [
-                {id:1, foodName: 'フォー', size: null, count: 1, price: 800, foodImage: 'pho.jpg'},
-                {id:2, foodName: 'ピリ辛ブンーボーフェ', size: null, count: 1, price: 800, foodImage: 'bunbohue.jpg'},
-                {id:3, foodName: '厚揚げブンと特性ソース付け', size: null, count: 1, price: 800, foodImage: 'bundau.jpg'},
-                {id:4, foodName: 'ベトナム風うどん', size: null, count: 1, price: 800, foodImage: 'chaocanh.jpg'},
-                {id:5, foodName: 'ベトナム風ピリ辛うなぎスープ', size: null, count: 1, price: 1800, foodImage: 'supluon.jpg'},
-                {id:6, foodName: 'ピザ', size: null, count: 1, price: 1200, foodImage: 'pizza.jpg'},
-                {id:7, foodName: 'サンドイッチ', size: null, count: 1, price: 210, foodImage: 'sandwich.jpg'},
-                {id:8, foodName: '引張うどん', size: null, count: 1, price: 500, foodImage: 'udon.jpg'},
-                {id:9, foodName: '柔らかライスペーパーの肉巻き', size: null, count: 1, price: 500, foodImage: 'banhcuon.jpg'},
-                {id:10, foodName: '柔らかライスペーパーセット', size: null, count: 1, price: 300, foodImage: 'banhmuot.jpg'},
-                {id:11, foodName: '特別なバンミー', size: null, count: 1, price: 1000, foodImage: 'banhmidacbiet.jpg'},
-                {id:12, foodName: 'ハム入りバンミー', size: null, count: 1, price: 600, foodImage: 'banhmido.jpg'},
-                {id:13, foodName: '焼肉バンミー', size: null, count: 1, price: 650, foodImage: 'banhmithitquay.jpg'},
-                {id:14, foodName: 'レバーミックスバンミー', size: null, count: 1, price: 500, foodImage: 'banhmybate.jpg'},
-                {id:15, foodName: '焼肉バンミー（特性ピリ辛ソース入り）', size: null, count: 1, price: 700, foodImage: 'banhmysotcay.jpg'},
-                {id:16, foodName: '特性ミックスバンミー', size: null, count: 1, price: 850, foodImage: 'banhmythamcam.jpg'},
-                {id:17, foodName: 'ドーナツセット', size: null, count: 1, price: 700, foodImage: 'donatset.jpg'},
-                {id:18, foodName: '焼き餃子（20個入り）', size: null, count: 1, price: 500, foodImage: 'gyoza.jpg'},
-                {id:19, foodName: 'ハンバーガーセット', size: null, count: 1, price: 1300, foodImage: 'humbagerset.jpg'},
-                {id:20, foodName: 'ミートソースパスター', size: null, count: 1, price: 600, foodImage: 'mitososu.jpg'},
-                {id:21, foodName: 'コーンのおこわセット', size: null, count: 1, price: 550, foodImage: 'xoibap.jpg'},
-                {id:22, foodName: '緑豆おこわセット', size: null, count: 1, price: 550, foodImage: 'xoidauxanh.jpg'},
-                {id:23, foodName: 'ピーナッツおこわセット', size: null, count: 1, price: 550, foodImage: 'xoilac.jpg'},
+                {id:1, foodName: 'フォー', size: null, count: '1', price: 800, foodImage: 'pho.jpg'},
+                {id:2, foodName: 'ピリ辛ブンーボーフェ', size: null, count: '1', price: 800, foodImage: 'bunbohue.jpg'},
+                {id:3, foodName: '厚揚げブンと特性ソース付け', size: null, count: '1', price: 800, foodImage: 'bundau.jpg'},
+                {id:4, foodName: 'ベトナム風うどん', size: null, count: '1', price: 800, foodImage: 'chaocanh.jpg'},
+                {id:5, foodName: 'ベトナム風ピリ辛うなぎスープ', size: null, count: '1', price: 1800, foodImage: 'supluon.jpg'},
+                {id:6, foodName: 'ピザ', size: null, count: '1', price: 1200, foodImage: 'pizza.jpg'},
+                {id:7, foodName: 'サンドイッチ', size: null, count: '1', price: 210, foodImage: 'sandwich.jpg'},
+                {id:8, foodName: '引張うどん', size: null, count: '1', price: 500, foodImage: 'udon.jpg'},
+                {id:9, foodName: '柔らかライスペーパーの肉巻き', size: null, count: '1', price: 500, foodImage: 'banhcuon.jpg'},
+                {id:10, foodName: '柔らかライスペーパーセット', size: null, count: '1', price: 300, foodImage: 'banhmuot.jpg'},
+                {id:11, foodName: '特別なバンミー', size: null, count: '1', price: 1000, foodImage: 'banhmidacbiet.jpg'},
+                {id:12, foodName: 'ハム入りバンミー', size: null, count: '1', price: 600, foodImage: 'banhmido.jpg'},
+                {id:13, foodName: '焼肉バンミー', size: null, count: '1', price: 650, foodImage: 'banhmithitquay.jpg'},
+                {id:14, foodName: 'レバーミックスバンミー', size: null, count: '1', price: 500, foodImage: 'banhmybate.jpg'},
+                {id:15, foodName: '焼肉バンミー（特性ピリ辛ソース入り）', size: null, count: '1', price: 700, foodImage: 'banhmysotcay.jpg'},
+                {id:16, foodName: '特性ミックスバンミー', size: null, count: '1', price: 850, foodImage: 'banhmythamcam.jpg'},
+                {id:17, foodName: 'ドーナツセット', size: null, count: '1', price: 700, foodImage: 'donatset.jpg'},
+                {id:18, foodName: '焼き餃子（20個入り）', size: null, count: '1', price: 500, foodImage: 'gyoza.jpg'},
+                {id:19, foodName: 'ハンバーガーセット', size: null, count: '1', price: 1300, foodImage: 'humbagerset.jpg'},
+                {id:20, foodName: 'ミートソースパスター', size: null, count: '1', price: 600, foodImage: 'mitososu.jpg'},
+                {id:21, foodName: 'コーンのおこわセット', size: null, count: '1', price: 550, foodImage: 'xoibap.jpg'},
+                {id:22, foodName: '緑豆おこわセット', size: null, count: '1', price: 550, foodImage: 'xoidauxanh.jpg'},
+                {id:23, foodName: 'ピーナッツおこわセット', size: null, count: '1', price: 550, foodImage: 'xoilac.jpg'},
             ],
             invalidSelect: [
-                { text: '特盛', value: 0 },
-                { text: '大盛', value: 1 },
-                { text: '中盛', value: 2 },
-                { text: '並盛', value: 3 },
+                { text: '特盛', value: 150 },
+                { text: '大盛', value: 100 },
+                { text: '中盛', value: 50 },
+                { text: '並盛', value: 1 },
             ],
             invalidCountSelect: [
                 { text: '1', value: 1 },
@@ -134,7 +134,9 @@ export default {
         oder_items: {
             handler: function (newData) {
                 if (!newData) return
-                this.tabulator.replaceData(newData)
+                this.tabulator?.replaceData(newData)
+                this.tabulator?.redraw()
+                this.calculationPayment(newData)
             },
             deep: true
         }
@@ -146,14 +148,13 @@ export default {
             const invalidParams = this.invalidSelect.reduce((pre, cur) => Object.assign(pre, { [cur.text]: cur.text }), {})
             const invalidCountParams = this.invalidCountSelect.reduce((pre, cur) => Object.assign(pre, { [cur.text]: cur.text }), {})
             const baseColumn = [
-                { rowHandle: true, formatter:'handle', frozen: true, width: 30 },
                 { title: '品名', field: 'foodName', width: 230, },
                 { title: 'サイズ', field: 'size', width: 90, editor:"select", editorParams: { values: invalidParams } },
                 { title: '個数', field: 'count', width: 80, editor:"select", editorParams: { values: invalidCountParams } },
                 { title: '金額', field: 'price', width: 100}
             ]
             this.options.columns = baseColumn.concat(this.actionColumn)
-            this.options.data = this.oder_items
+            // this.options.data = this.oder_items
             this.tabulator = new Tabulator(this.$refs.table, this.options);
             this.cellClick(this.tabulator)
             this.dataChanged(this.tabulator)
@@ -164,18 +165,13 @@ export default {
     methods: {
         dataChanged(table) {
             table.on("dataChanged", (data) => {
-                data.reduce((val) => {
-                    console.log(val)
-                })
-                // this.oder_items = data
+                this.oder_items = data
             }, this);
-            this.calculationPayment(this.oder_items)
         },
         cellClick(table) {
             table.on('cellClick', (e, cell) => {
                 const methodsName = e.target.getAttribute('call-methods')
                 const row = cell.getRow()
-                const rowData = row.getData()
                 switch (methodsName) {
                     case 'onDelete':
                         this.onDelete(row);
@@ -187,6 +183,7 @@ export default {
             const isExisted = this.oder_items.some(item => item.id === food.id)
             if (!isExisted) {
                 this.oder_items.push(food)
+                this.tabulator.addData(food, true)
                 this.calculationPayment(this.oder_items)
             }
         },
@@ -203,9 +200,12 @@ export default {
             }
         },
         calculationPayment(rowDatas) {
-            this.total = rowDatas.reduce((sumPrice, value) => sumPrice + parseInt(value.price), 0)
+            this.total = rowDatas.reduce((sumPrice, value) => sumPrice + (parseInt(value.price) * parseInt(value.count)), 0)
             this.taxPrice = this.total * 0.1
             this.itemPrice = this.total + this.taxPrice
+        },
+        invalidSelectTextToValue(invalidSelectText) {
+            return this.invalidSelect.find(item => item.text === invalidSelectText )?.value ?? invalidSelectText
         }
     },
 }
